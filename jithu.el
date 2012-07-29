a7da68bc95fdd16f33921bf48f4dd3ae5086becf
Jithu Gopal <jithug87@gmail.com>
2012-07-30 00:02:51 +0530

    me me me....

---
+++ jithu.el
@@ -0,0 +1,9 @@
+(color-theme-sanityinc-solarized-dark)
+
+(menu-bar-mode t)
+
+(global-auto-revert-mode t)
+
+(set-face-attribute 'default nil
+                    :family "Inconsolata"
+                    :height 160)
